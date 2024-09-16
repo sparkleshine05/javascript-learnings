145247353f7be562722e2790db5f8fec938689af
sparkleshine05 <105545774+sparkleshine05@users.noreply.github.com>
2024-09-16 12:56:32 +0000

    learnings in JS

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "Mukti"
+
+const repoCount = 50
+
+console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Mukti Bhargava');
+
+//console.log(gameName);
+
+//console.log(typeof gameName);
+
+//console.log(gameName[0]);
+
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+
+//console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+
+
