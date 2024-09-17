6b9534a252301b5d677f71d001fd7d7a79c8e791
sparkleshine05 <105545774+sparkleshine05@users.noreply.github.com>
2024-09-17 10:36:21 +0000

    learnings in JS

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,50 @@
+//singleton through constructors, a singleton is always made
+
+
+//object literals
+
+
+const mySymbol = Symbol("key1")
+
+const JsUser = {
+    name: "Mukti",
+    "full name": "Mukti Bhargava",
+    [mySymbol]: "myKey1",
+    age: 37,
+    location: "Pune",
+    email: "mukti.b@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySymbol]);
+// console.log(typeof JsUser.mySymbol);
+
+
+JsUser.email = "mukti.bhargava05@yahoo.com";
+//console.log(JsUser["email"])
+//Object.freeze(JsUser);
+
+JsUser.email = "mukti.bhargava05@microsoft.com";
+// console.log(JsUser["email"]);
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+    
+}
+
+console.log(JsUser.greeting); //function reference
+console.log(JsUser.greeting());
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+    
+}
+
+console.log(JsUser.greetingTwo());
