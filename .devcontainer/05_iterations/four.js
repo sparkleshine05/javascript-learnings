e9d6c364ba35899bbf88b50b3512865c69913115
sparkleshine05 <105545774+sparkleshine05@users.noreply.github.com>
2024-10-08 22:26:38 +0000

    loops in js

---
+++ .devcontainer/05_iterations/four.js
@@ -0,0 +1,25 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    py: 'python',
+    rb: 'ruby'
+}
+
+for(const key in myObject){
+    //console.log(`${key} shortcut id for ${myObject[key]}`);
+    
+}
+
+const arr = ["js","rb","py","java","cpp"]
+
+for(const key in arr){
+   // console.log(key);
+    
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for(const key in programming){
+    console.log(programming[key]);
+    
+}
